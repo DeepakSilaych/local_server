3de41fa0cc375fd65d4c50136db980ea35a764f6
DeepakSilaych <deepaksilaych@gmail.com>
2024-06-12 02:27:10 +0530

    ohoho

--- server/urls.py
+++ server/urls.py
@@ -3,8 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogs/', include('blogs.urls')),
-    path('cs/', include('crowdsource.urls')),
     path('aws/', include('awsstations.urls')),
-    path('weather/', include('weatherstations.urls')),
 ]
